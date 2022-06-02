ebefd099be635145e80a80838e88e8ddb0dae659
Rahul Agarwal <rahulaga@amazon.com>
2022-06-01 18:00:55 -0700

    Add next config.

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const withSass = require("@zeit/next-sass");
+module.exports = withSass({
+  cssModules: true,
+});
+module.exports = {
+  /* Add Your Scss File Folder Path Here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
